2e5a110b6e018516fe372f30b39d66852c371ac4
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-02-07 14:05:57 +0000

    code update

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 
-
-
+import time
+import pyautogui
+from PIL import ImageGrab
+from threading import Thread
 
 
 def main():
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hello world")  # Press ⌘F8 to toggle the breakpoint.
+    thread = Thread(target=screenshotT1)
+    thread.start()
+
+
+def screenshotT1():
+    # wait three seconds
+    time.sleep(5)
+    # take a screenshot    
+    im1 = pyautogui.screenshot()
+    # save it
+    im1.save('my_screenshot.png')
+    # send it to a server
+    
+    
+
+
+def screenshotT2():
+    myscreen = ImageGrab.grab()
+    myscreen.save('myscreen_PIL.png')
 
 
 main()
-
